d4fe5a0c1d83c39603391bd31f35ff4fbcebd696
andrew950468 <andrewho950468@berkeley.edu>
2015-06-17 07:03:57 -0700

    add notes to the page_controller

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,12 +3,14 @@ class PageController < ApplicationController
 
   def new
   end
-
+  #index is the homepage
   def index
+
   	#@page = @graph.get_object("1631974847020962")
   	puts "current user #{current_user.inspect}"
     puts "pages #{@pages.inspect}"
 
+    #get pages on the user's behalf
   	@pages = graph.get_connections('me', 'accounts') #page accounts
   end
 
@@ -30,6 +32,7 @@ class PageController < ApplicationController
     page_token = graph.get_page_access_token(params[:page_id])
     page_graph = Koala::Facebook::API.new(page_token)
 
+    #pass in messages
     status = page_graph.put_wall_post(params[:message])
     puts status.inspect
 
