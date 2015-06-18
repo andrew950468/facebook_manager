8425fd86cecffa941ad62bc195b35a0bf76f03e2
andrew950468 <andrewho950468@berkeley.edu>
2015-06-17 17:06:43 -0700

    listing unpublished posts

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -22,6 +22,8 @@ class PageController < ApplicationController
 	  @page_graph = Koala::Facebook::API.new(page_token)
     @page = @page_graph.get_object("me")
   	@feed_items = @page_graph.get_connection('me', 'feed') 
+    @unpublished_items = @page_graph.get_connection('me', 'promotable_posts')
+    puts "unpublished items: #{@unpublished_items.inspect}"
   end
 
   def create
@@ -31,6 +33,9 @@ class PageController < ApplicationController
     graph = Koala::Facebook::API.new(current_user.token)
     page_token = graph.get_page_access_token(params[:page_id])
     page_graph = Koala::Facebook::API.new(page_token)
+
+
+    insights = @page_graph.get_connections(page_id, 'insights/page_impressions_frequency_distribution', period: 'week')
 
     #pass in messages
     status = page_graph.put_wall_post(params[:message])
